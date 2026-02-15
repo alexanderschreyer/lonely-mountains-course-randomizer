20b71c3b473e6a8cff6ae3899c654b6a31454b46
Alexander Schreyer <alexander.schreyer@students.fhnw.ch>
2026-02-15 20:40:32 +0100

    made printing the randomized courseList a separate method

--- src/CourseRandomizer.java
+++ src/CourseRandomizer.java
@@ -6,15 +6,25 @@ import java.util.List;
 public class CourseRandomizer {
     public static void main(String[] args) {
         CourseSelector courseSelector = new CourseSelector();
+        courseSelector.setIncludeDlc(true);
         List<Course> courseList = courseSelector.generateRandomCourseList();
+        printTourList(courseList);
+    }
 
-        int index = 0;
+    public static void printTourList(List<Course> courseList) {
+        int tourIndex = 1;
+        int courseIndex = 0;
+
         for(Course course : courseList) {
-            index++;
-            System.out.print(index + ": " + course.getCourseName() + " [" + course.getMountainName() + "] \n");
-            if (index % 4 == 0) {
+            if (courseIndex == 0) {
+                System.out.print("Tour " + tourIndex + "\n");
+                tourIndex++;
+            }
+            courseIndex++;
+            System.out.print(courseIndex + ": " + course.getCourseName() + " [" + course.getMountainName() + "] \n");
+            if (courseIndex % 4 == 0) {
                 System.out.print("\n");
-                index = 0;
+                courseIndex = 0;
             }
         }
     }
